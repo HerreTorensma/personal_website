6ff764138fc480b1c62c0422f4db27f99af3f5a1
HerreTorensma <herre@torensma.net>
2024-06-14 00:49:30 +0200

    Add media page

---
+++ media/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+import yaml
+
+media = Blueprint("media", __name__)
+
+@media.get("/")
+def media_page():
+    # Scrape movies from IMDB
+    movies = []
+    with open("static/media/movie_info.yaml") as file:
+        movies = yaml.load(file)
+
+    # ia = IMDb()
+
+    # goodfellas = ia.get_movie("0099685")
+    # goodfellas.url = f"https://www.imdb.com/title/tt{goodfellas.movieID}/"
+    # movies.append(goodfellas)
+
+    return render_template("media.html", movies=movies)
--- server.py
+++ server.py
@@ -5,9 +5,11 @@ def create_app():
 
     from main.routes import main
     from blog.routes import blog
+    from media.routes import media
     
     app.register_blueprint(main)
     app.register_blueprint(blog, url_prefix="/blog")
+    app.register_blueprint(media, url_prefix="/media")
 
     return app
 
