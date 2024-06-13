dc389c3655ee90b3a6567d8f435f96566adb562a
HerreTorensma <herre@torensma.net>
2024-06-14 00:58:06 +0200

    Fix more

--- media/routes.py
+++ media/routes.py
@@ -3,17 +3,11 @@ import yaml
 
 media = Blueprint("media", __name__)
 
-@media.get("/")
+@media.route("/")
 def media_page():
-    # Scrape movies from IMDB
     movies = []
+
     with open("static/media/movie_info.yml", "r") as file:
         movies = yaml.load(file)
-
-    # ia = IMDb()
-
-    # goodfellas = ia.get_movie("0099685")
-    # goodfellas.url = f"https://www.imdb.com/title/tt{goodfellas.movieID}/"
-    # movies.append(goodfellas)
 
     return render_template("media.html", movies=movies)
