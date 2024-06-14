91c0e6b1d52cf7bceaa55a96a470de507c1dc6ee
HerreTorensma <herre@torensma.net>
2024-06-14 14:00:17 +0200

    Fix yaml load error

--- media/routes.py
+++ media/routes.py
@@ -8,6 +8,6 @@ def media_page():
     movies = []
 
     with open("static/media/movie_info.yml", "r") as file:
-        movies = yaml.load(file)
+        movies = yaml.safe_load(file)
 
     return render_template("media.html", movies=movies)
